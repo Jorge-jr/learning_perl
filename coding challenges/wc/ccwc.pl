f8af45f8333cfb505e627c7b73a9f57529ebd8b0
Jorge-jr <jsribeirojr@hotmail.com>
2023-10-26 17:16:57 -0300

    implemented the -m (byte count) functionality

--- coding challenges/wc/ccwc.pl
+++ coding challenges/wc/ccwc.pl
@@ -4,16 +4,19 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use Encode;
 
 my $count_chars;
 my $count_lines;
 my $count_words;
+my $count_bytes;
 
 # Define command-line options
 GetOptions(
     'c' => \$count_chars, # -c for character count
     'l' => \$count_lines, # -l for line count
-    'w' => \$count_words  # -w for word count
+    'w' => \$count_words,  # -w for word count
+    'm' => \$count_bytes  # -m for byte count
 );
 
 my $filename = shift @ARGV; # Get the filename from the remaining arguments
@@ -21,26 +24,30 @@ my $filename = shift @ARGV; # Get the filename from the remaining arguments
 my $line_count = 0;
 my $word_count = 0;
 my $char_count = 0;
+my $byte_count = 0;  # Initialize byte count
 
-if (open my $filehandle, '<', $filename) {
+if (open my $filehandle, '<:raw', $filename) {  # Open the file in binary mode
     while (my $line = <$filehandle>) {
         $line_count += 1;
         $word_count += scalar split(" ", $line);
-        $char_count += length $line;
+        $char_count += length decode_utf8($line);  # Calculate character count
+        $byte_count += length $line;  # Calculate byte count
     }
     close $filehandle;
 } else {
     die "Failed to open the file: $!";
-}
+} 
 
 if ($count_chars) {
     print "$char_count $filename\n";
-}
-
-if ($count_lines) {
+} elsif ($count_lines) {
     print "$line_count $filename\n";
-}
-
-if ($count_words) {
+}elsif ($count_words) {
     print "$word_count $filename\n";
+} elsif ($count_chars) {
+    print "$char_count $filename\n";
+} elsif ($count_bytes) {
+    print "$byte_count $filename\n";
+} else {
+    print "$char_count $line_count $word_count $byte_count $filename\n";
 }
