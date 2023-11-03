358133c358b73d916b0d71131e4830fc2069a8a0
Jorge-jr <jsribeirojr@hotmail.com>
2023-11-03 20:53:56 -0300

    Update heap_sort.pl

--- sorting/heap_sort.pl
+++ sorting/heap_sort.pl
@@ -1,32 +1,50 @@
-def heapify(sequence, size, index):
+use strict;
+use warnings;
 
+sub heapify {
+    my ($sequence, $size, $index) = @_;
+    my $left = $index * 2 + 1;
+    my $right = $index * 2 + 2;
+    my $largest = $index;
 
-    largest = index
-    left = 2 * index + 1
-    right = 2 * index + 2
-    print(f"checking {sequence}\n index:{index}\n branches:{left, right}")
+    if ($left < $size && $sequence->[$left] > $sequence->[$largest]) {
+        $largest = $left;
+    }
 
-    if left < size and sequence[left] > sequence[largest]:
-        largest = left
+    if ($right < $size && $sequence->[$right] > $sequence->[$largest]) {
+        $largest = $right;
+    }
 
-    if right < size and sequence[right] > sequence[largest]:
-        largest = right
+    if ($largest != $index) {
+        ($sequence->[$largest], $sequence->[$index]) = ($sequence->[$index], $sequence->[$largest]);
+        heapify($sequence, $size, $largest);
+    }
+}
 
+sub heapsort {
+    my @sequence = @_;
+    my $size = scalar @sequence;
 
-    if largest != index:
-        sequence[index], sequence[largest] = sequence[largest], sequence[index]
-        heapify(sequence, size, largest)
-    
+    # Turning the sequence into a max_heap
+    for (my $i = int($size / 2) - 1; $i >= 0; $i--) {
+        heapify(\@sequence, $size, $i);
+    }
 
-def heap_sort(sequence):
+    # Sorting time! The max-heap's first element is certainly the largest, so it goes straight to the end.
+    # Then, we heapify it again to keep it as a max-heap.
+    # Each time we heapify, the largest element of the sub-tree goes to the first position.
+    # So we just swap it with the last position of the sub-tree.
+    # Repeat it until it's sorted.
 
-    size = len(sequence)
+    for (my $i = $size - 1; $i > 0; $i--) {
+        ($sequence[0], $sequence[$i]) = ($sequence[$i], $sequence[0]);
+        heapify(\@sequence, $i, 0);
+    }
 
-    for i in range(size//2-1, -1, -1):
-        heapify(sequence, size, i)
+    return @sequence;
+}
 
-    for i in range(size-1, 0, -1):
-        sequence[i], sequence[0] = sequence[0], sequence[i]
-        heapify(sequence, i, 0)
+my @arr = (3, 7, 0, -4, 1, 55);
+my @sorted = heapsort(@arr);
 
-    return sequence
+print "Sorted array: @sorted\n";
